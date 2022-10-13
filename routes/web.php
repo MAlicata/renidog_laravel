76bd9b79ae38aa019b46df65bf7bdf83976afa5f
Matias Jesus Alicata <matiasalicata@gmail.com>
2022-10-13 13:38:04 -0300

    add routes with names

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::view('/about', 'about');
-Route::view('/galery', 'galery');
-Route::view('/contact', 'contact');
+})->name('welcome');
+Route::view('/about', 'about')->name('about');
+Route::view('/galery', 'galery')->name('galery');
+Route::view('/contact', 'contact')->name('contact');
